58c62e4265ebf759823ffc61483768e22186a42b
Artem <grynbest@gmail.com>
2023-04-14 02:56:45 +0300

    link fix

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { App } from 'components/App';
 import './index.css';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './components/theme.jsx';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import GlobalStyle from 'components/Global';
 
 
@@ -12,10 +12,10 @@ import GlobalStyle from 'components/Global';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <HashRouter >
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <GlobalStyle />
         <App />
-      </HashRouter>
+      </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>
 );
