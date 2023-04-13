7d13a8086f84dbaa49e3a4d9504198371b51653e
Artem <grynbest@gmail.com>
2023-04-13 23:46:03 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Navigate, Outlet, useLocation } from 'react-router-dom';
+import { Navigate, useLocation } from 'react-router-dom';
 import React, { lazy, Suspense } from 'react';
 import GlobalStyle from './Global';
 
