54b888b58cb5d998d41df704459c672748ae179b
Artem <grynbest@gmail.com>
2023-04-25 22:36:46 +0300

    fixed if the matrix disaplying if the screen width less than 400px

--- src/components/CalculatorPageComponents/PersonalMatrix/MatrixCalculation/FateMatrix/MatrixGraph/MatrixGraph.styled.jsx
+++ src/components/CalculatorPageComponents/PersonalMatrix/MatrixCalculation/FateMatrix/MatrixGraph/MatrixGraph.styled.jsx
@@ -4,6 +4,9 @@ import { ReactComponent as Matrix } from 'images/Calculator/personalMatrix/karma
 export const MatrixImg = styled(Matrix)`
   width: 370px;
   height: auto;
+  @media screen and (max-width: ${p => p.theme.sizes.mobile}) {
+    width: 100%;
+  }
   @media screen and (min-width: ${p => p.theme.sizes.tablet}) {
     width: 667px;
   }
